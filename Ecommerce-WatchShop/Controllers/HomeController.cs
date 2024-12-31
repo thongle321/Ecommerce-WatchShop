0906ea778ab8b174b6e9c6cce9d33317df262ab1
qkhanhcfaz <tranvanquockhanh123@gmail.com>
2024-12-31 15:27:32 +0700

    Chỉnh sửa đường link tới các trang

--- Ecommerce-WatchShop/Controllers/HomeController.cs
+++ Ecommerce-WatchShop/Controllers/HomeController.cs
@@ -25,6 +25,10 @@ public class HomeController : Controller
     {
         return View();
     }
+    public IActionResult Favorite()
+    {
+        return View();
+    }
     public IActionResult Privacy()
     {
         return View();
