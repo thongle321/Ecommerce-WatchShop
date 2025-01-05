5a2bf31c19cbe0c3724455d708414afffb35e0b8
ThongLe <thongle857@gmail.com>
2025-01-05 17:05:12 +0700

    Thêm chức năng đăng nhập và đăng ký trong controller HomeController

--- Ecommerce-WatchShop/Controllers/HomeController.cs
+++ Ecommerce-WatchShop/Controllers/HomeController.cs
@@ -1,16 +1,23 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Ecommerce_WatchShop.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+using Ecommerce_WatchShop.Models.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace Ecommerce_WatchShop.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    private readonly DongHoContext _context;
+    public HomeController(ILogger<HomeController> logger, DongHoContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
@@ -29,6 +36,88 @@ public class HomeController : Controller
     {
         return View();
     }
+    [HttpPost]
+    public async Task<IActionResult> Register(RegisterVM registerVM)
+    {
+        if (!ModelState.IsValid)
+        {
+            //return PartialView("_RegisterPartial", registerVM);
+        }
+        var existingUser = _context.Accounts
+            .FirstOrDefault(a => a.Username == registerVM.Username);
+
+        if (existingUser != null)
+        {
+            //ModelState.AddModelError("Username", "Username đã tồn tại.");
+            //return PartialView("_RegisterPartial", registerVM);
+        }
+
+        var account = new Account
+        {
+            Username = registerVM.Username,
+            Password = registerVM.Password,
+            RoleId = 1,
+        };
+
+        _context.Accounts.Add(account);
+        await _context.SaveChangesAsync();
+
+        var customer = new Customer
+        {
+            AccountId = account.AccountId,
+        };
+
+        _context.Customers.Add(customer);
+        await _context.SaveChangesAsync();
+
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, account.Username),
+            new Claim(ClaimTypes.Role, "User")
+        };
+
+        var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+        var authProperties = new AuthenticationProperties
+        {
+            IsPersistent = true
+        };
+
+        await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authProperties);
+
+        return RedirectToAction("Index");
+    }
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginVM loginVM)
+    {
+        if (!ModelState.IsValid)
+        {
+            //return PartialView("_LoginPartial", loginVM);
+        }
+        var account = await _context.Accounts.FirstOrDefaultAsync(a => a.Username == loginVM.Username);
+
+        if (account == null || account.Password != loginVM.Password)
+        {
+            TempData["LoginError"] = "Tài khoản hoặc mật khẩu không đúng.";
+        }
+
+        var claims = new List<Claim>
+        {
+        new Claim(ClaimTypes.Name, account.Username),
+        new Claim(ClaimTypes.Role, "User")
+        };
+
+        var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+        var authProperties = new AuthenticationProperties
+        {
+            IsPersistent = true
+        };
+
+        await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authProperties);
+
+        return RedirectToAction("Index", "Home");
+    }
+
+
     public IActionResult Privacy()
     {
         return View();
--- Ecommerce-WatchShop/Program.cs
+++ Ecommerce-WatchShop/Program.cs
@@ -8,6 +8,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<DongHoContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddAuthentication("Cookies")
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Home/Login";
+        options.AccessDeniedPath = "/Home/AccessDenied";
+    });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,6 +27,7 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapStaticAssets();
 app.MapAreaControllerRoute(
