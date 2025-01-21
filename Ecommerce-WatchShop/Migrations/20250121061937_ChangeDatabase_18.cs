1a265d77b106d4ed644a8ca4b41f9ae89859b1ff
thongle321 <thongle857@gmail.com>
2025-01-21 15:18:29 +0700

    Cập nhật

---
+++ Ecommerce-WatchShop/Migrations/20250121061937_ChangeDatabase_18.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecommerce_WatchShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeDatabase_18 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Contents",
+                table: "Brands");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Contents",
+                table: "Brands",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
